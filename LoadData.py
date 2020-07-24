3915c3836ba1cbfc9f4284c21bbcec3507fd4e1e
iBraven <ciubotariu.luciano@gmail.com>
2020-07-24 21:07:21 +0300

    ---

--- LoadData.py
+++ LoadData.py
@@ -1,1 +1,14 @@
 #Load Data
+import torch
+import torchvision as tv
+
+image_path = "cale\\catre\\folder\\img"
+image_size = {"width": 256, "height": 256}
+
+
+class ImageLoader:
+    def __init__(self, batch_size=32):
+        self.dataset = tv.datasets.ImageFolder(image_path, transform=None)
+
+    def return_data(self):
+        return self.dataset
--- Main.py
+++ Main.py
@@ -1,1 +1,4 @@
 #Main
+import torch as th
+print(f'version: {th.__version__}\ncuda available: {th.cuda.is_available()}')
+
