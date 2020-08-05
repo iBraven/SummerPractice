2fbce56ba877d731169ffe30131917cc0c9f055f
Ciubotariu-Luciano <narcis-luciano.ciubotariu@student.tuiasi.ro>
2020-08-05 13:54:30 +0300

    Err loss - Nan

--- nnTrain.py
+++ nnTrain.py
@@ -6,17 +6,19 @@ import torch.nn as nn
 import LoadData
 import nnModel
 
-train_loader, test_loader = LoadData.load_data(LoadData.path, nr_rows=100, transform=LoadData.transform)
+batch_size = 8
+n_rows = None
+train_loader, test_loader = LoadData.load_data(LoadData.path, nr_rows=n_rows, transform=LoadData.transform, batch_size=batch_size)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# torch.backends.cudnn.enabled=False
-# torch.backends.cudnn.benchmark=True
+torch.backends.cudnn.enabled = False
+torch.backends.cudnn.benchmark = True
 
 net = nnModel.Net()
 net = net.to(device)
-criterion = nn.SmoothL1Loss()
+criterion = nn.MultiLabelSoftMarginLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001)
 
-for epoch in range(1):  # loop over the dataset multiple times
+for epoch in range(2):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
@@ -31,16 +33,16 @@ for epoch in range(1):  # loop over the dataset multiple times
         # print(type(outputs), type(points), type(image))
         # break
         loss = criterion(outputs, points)
-        print(loss.dtype)
+        # print(loss.dtype)
         # break
         loss.backward()
         optimizer.step()
 
         # print statistics
         running_loss += loss.item()
-        if i % 10 == 9:    # print every 10 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 10))
+        if i % 100 == 99:    # print every 10 mini-batches
+            print('[%d, %5d] loss: %.4f' %
+                  (epoch + 1, (i + 1)*batch_size, running_loss / 100))
             running_loss = 0.0
 
 print('Finished Training')
